0e03b58dee45afcf7d05ed7279fb53107656fe4d
Michal Boska <michal.boska@mimacom.com>
2016-02-23 16:55:30 +0100

    First dummy service

--- src/main/java/com/github/michalboska/vertx3/gcm/internal/GcmServiceImpl.java
+++ src/main/java/com/github/michalboska/vertx3/gcm/internal/GcmServiceImpl.java
@@ -1,15 +1,18 @@
 package com.github.michalboska.vertx3.gcm.internal;
 
 import com.github.michalboska.vertx3.gcm.GcmService;
+import com.github.michalboska.vertx3.gcm.GcmServiceVertxEBProxy;
+import com.github.michalboska.vertx3.gcm.GcmServiceVertxProxyHandler;
 import com.github.michalboska.vertx3.gcm.SomeDto;
 import io.vertx.core.Vertx;
+import io.vertx.serviceproxy.ProxyHelper;
 
 public class GcmServiceImpl implements GcmService {
 
-    private Vertx vertx;
+    private GcmServiceVertxProxyHandler _handler;
 
     public GcmServiceImpl(Vertx vertx) {
-        this.vertx = vertx;
+        GcmServiceVertxProxyHandler handler = new GcmServiceVertxProxyHandler(vertx, this);
     }
 
     @Override
